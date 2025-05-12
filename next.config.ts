341c74a286ff0eb4ecf006dbbba52a0a722b1660
Chintan Patel <patelchintan8358@gmail.com>
2025-05-12 10:33:21 +0000

    images: { remotePatterns: [       {         protocol: "https",         hostname: "**",       },     ],     domains: ['eshop-test1.s3.ap-south-1.amazonaws.com'], }
    this type

--- next.config.ts
+++ next.config.ts
@@ -13,28 +13,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: '**', // Allows all hostnames for images served over HTTPS
       },
-      {
-        protocol: 'https',
-        hostname: 'eshop-test1.s3.ap-south-1.amazonaws.com', 
-      },
-      {
-        protocol: 'https',
-        hostname: '*.s3.amazonaws.com', // General pattern for S3
-      },
-       {
-        protocol: 'https',
-        hostname: '*.s3.*.amazonaws.com', // General pattern for region-specific S3
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com', 
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.samsung.com', // Added from previous request
-      }
     ],
   },
 };
